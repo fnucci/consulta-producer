8a84559390b403ece74346fadacf9375bd650cb6
Jonathan <jonathanutsuni@gmail.com>
2025-10-07 00:26:51 -0300

    Adicionando URL de autenticação configurável docker

--- src/main/java/fiap/tech/challenge/marcador_consultas/consulta_producer/service/LoginService.java
+++ src/main/java/fiap/tech/challenge/marcador_consultas/consulta_producer/service/LoginService.java
@@ -2,6 +2,7 @@ package fiap.tech.challenge.marcador_consultas.consulta_producer.service;
 
 import fiap.tech.challenge.marcador_consultas.consulta_producer.dto.in.LoginRequest;
 import fiap.tech.challenge.marcador_consultas.consulta_producer.dto.out.LoginResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,23 +10,26 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class LoginService {
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-    public LoginService (RestTemplate restTemplate){
+    // injeta a URL configurável (pode vir do application.yml ou de variável de ambiente)
+    @Value("${auth.url}")
+    private String authUrl;
+
+    public LoginService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public String autenticar() {
-        String url = "http://localhost:8080/api/auth/login";
-
         LoginRequest request = new LoginRequest("enfermeiro", "guest");
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<LoginRequest> entity = new HttpEntity<>(request, headers);
 
         ResponseEntity<LoginResponse> response =
-                restTemplate.exchange(url, HttpMethod.POST, entity, LoginResponse.class);
+                restTemplate.exchange(authUrl, HttpMethod.POST, entity, LoginResponse.class);
 
         if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
             return response.getBody().token();
